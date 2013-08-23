dab4072ac047cbb3cf781bf945cd83ba70568551
Timothy Hahn <timyhahn@gmail.com>
2013-08-23 18:59:36 -0400

    Added distance calculation

--- parkinglot/views.py
+++ parkinglot/views.py
@@ -2,6 +2,7 @@ from parkinglot import app
 from flask import url_for, redirect, request, jsonify
 from database import db_session
 from models import ParkingLot
+from helpers import distance_in_miles
 from datetime import datetime
 
 
@@ -18,6 +19,19 @@ def get_all_lots():
     lots_dict['lots'] = lots_list
     return jsonify(lots_dict)
 
+@app.route('/lot/distance')
+def get_distance_lots():
+    distance = (float)(request.args['distance'])
+    latitude = (float)(request.args['latitude'])
+    longitude = (float)(request.args['longitude'])
+    lots = ParkingLot.query.all()
+    lots_dict = dict()
+    lots_list = list()
+    for lot in lots:
+        if distance_in_miles(latitude, longitude, lot.latitude, lot.longitude) <= distance:
+            lots_list.append(lot.info())
+    lots_dict['lots'] = lots_list
+    return jsonify(lots_dict)
 @app.route('/lot/<int:lot_id>', methods=['GET'])
 def get_lot(lot_id):
     lot = ParkingLot.query.get(lot_id)
